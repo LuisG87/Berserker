39b414f77e1d5d07e89ef2782c116944636f8266
Luis Gomez <luisg2249@gmail.com>
2024-10-25 11:36:06 -0300

    bRegex class

--- src/BerserkerStatic/bArrays.php
+++ src/BerserkerStatic/bArrays.php
@@ -4,98 +4,123 @@ class bArrays{
 
 
     /**
-     * 
+     * Changes the case of all keys in an array to uppercase
+     * @param array $array
+     * @return array
      */
-    public static function uppercase($array):array{
+    public static function uppercaseKeys($array):array{
         return array_change_key_case( $array, CASE_UPPER);
     }
 
     /**
-     * 
+     * Changes the case of all keys in an array to lowercase
+     * @param array $array
+     * @return array
      */
-    public static function lowercase($array):array{
+    public static function lowercaseKeys($array):array{
         return array_change_key_case( $array, CASE_LOWER);
     }
 
     /**
-     * 
+     * Split an array into chunks
+     * creates an array with subarrays with a max length specified by $length
+     * @param array $array
+     * @param int $length
+     * @param bool $preserve_keys
+     * @return array
      */
     public static function chunk(array $array, int $length, bool $preserve_keys = false): array{
         return array_chunk($array, $length, $preserve_keys);
     }
 
     /**
-     * 
+     * Return the values from a single column in the input array
+     * @param array $array
+     * @param int|string|null $column_key
+     * @param int|string|null $index_key
+     * @return array
      */
     public static function column(array $array, int|string|null $column_key, int|string|null $index_key = null): array{
         return array_column($array, $column_key, $index_key);
     }
 
     /**
-     * 
+     * Checks if a value exists in an array
+     * @param mixed $needle
+     * @param array $haystack
+     * @param bool $strict
+     * @return bool
      */
     public static function valueExists(mixed $needle, array $haystack, bool $strict = false): bool{
         return in_array($needle, $haystack, $strict);
     }
 
     /**
-     * 
+     * Checks if the given key or index exists in the array
+     * @param string|int|float|bool|null $key
+     * @param array $array
+     * @return bool
      */
     public static function keyExists(string|int|float|bool|null $key, array $array): bool{
         return array_key_exists($key, $array);
     }
 
     /**
-     * 
+     * Counts the occurrences of each distinct value in an array
+     * @param array $array
+     * @return array
      */
     public static function valuesOccurrences(array $array): array{
         return array_count_values($array);
     }
 
     /**
-     * 
+     * Sort an array in ascending order
+     * @param array
+     * @param int $flags -FLAG(@see sort native function)
+     * @return true - never fails
      */
     public static function ascendingSort(array &$array, int $flags = SORT_REGULAR): true{
         return sort($array, $flags);
     }
 
     /**
-     * 
+     * Sort an array in descending order
      */
     public static function descendingSort(array &$array, int $flags = SORT_REGULAR): true{
         return rsort($array, $flags);
     }
 
     /**
-     * 
+     * Sort an array by key in ascending order
      */
     public static function ascendingKeySort(array &$array, int $flags = SORT_REGULAR): true{
         return ksort($array, $flags);
     }
 
     /**
-     * 
+     * Sort an array by key in descending order
      */
     public static function descendingKeySort(array &$array, int $flags = SORT_REGULAR): true{
         return krsort($array, $flags);
     }
 
-    /**
-     * 
+     /**
+     * Return all the keys or a subset of the keys of an array
      */
     public static function keys(array $array, mixed $filter_value = null, bool $strict = false): array{
         return array_keys($array, $filter_value, $strict);
     }
 
     /**
-     * 
+     *  Gets the first key of an array
      */
     public static function firstKey(array $array): int|string|null{
         return array_key_first($array);
     }
 
     /**
-     * 
+     * Gets the last key of an array
      */
     public static function lastKey(array $array): int|string|null{
         return array_key_last($array);
---
+++ src/imethod/bRegex.php
@@ -0,0 +1,76 @@
+<?php
+class bRegex{
+
+    public function __construct(){
+
+    }
+
+
+    /**
+     * Perform a regular expression match
+     */
+    public function match(string $pattern, string $subject, array &$matches = null, int $flags = 0, int $offset = 0): int|false{
+        return preg_match($pattern, $subject, $matches, $flags, $offset);
+    }
+
+    /**
+     * Perform a global regular expression match
+     */
+    public function matchAll(string $pattern, string $subject, array &$matches = null, int $flags = 0, int $offset = 0): int|false{
+        return preg_match_all($pattern, $subject, $matches, $flags, $offset);
+    }
+
+    /**
+     * Return array entries that match the pattern
+     */
+    public function grep(string $pattern, array $array, int $flags = 0): array|false{
+        return preg_grep($pattern, $array, $flags);
+    }
+
+    /**
+     * Quote regular expression characters
+     */
+    public function quote(string $str, ?string $delimiter = null): string{
+        return preg_quote($str, $delimiter);
+    }
+
+    /**
+     * Perform a regular expression search and replace
+     */
+    public function filter(string|array $pattern, string|array $replacement, string|array $subject, int $limit = -1, int &$count = null): string|array|null{
+        return preg_filter($pattern,$replacement,$subject,$limit,$count);
+    }
+
+    /**
+     * Perform a regular expression search and replace
+     */
+    public function replace(string|array $pattern, string|array $replacement, string|array $subject, int $limit = -1, int &$count = null): string|array|null{
+        return preg_replace($pattern,$replacement,$subject,$limit,$count);
+    }
+
+    /**
+     * Split string by a regular expression
+     */
+    public function split( string $pattern, string $subject, int $limit = -1, int $flags = 0): array|false{
+        return preg_split( $pattern, $subject, $limit, $flags);
+    }
+
+    /**
+     * Returns the error code of the last PCRE regex execution
+     * @return int - this integer values are represented by constants 
+     * @link https://www.php.net/manual/en/function.preg-last-error.php
+     */
+    public function lastError(): int{
+        return preg_last_error();
+    }
+
+    /**
+     * Returns the error message of the last PCRE regex execution
+     * @return string
+     */
+    public function lastErrorMsg(): string{
+        return preg_last_error_msg();
+    }
+}
+global $bRegex;
+$bRegex = new bRegex();
